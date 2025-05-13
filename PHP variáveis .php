31db1f3b87d6668a84e3a5336e6fe73aaa829768
felipekeller08 <felipeborile@gmail.com>
2025-05-13 11:43:55 -0300

    Add files via upload

---
+++ PHP variáveis .php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ //$txt = "alexandra";
+ //echo "bom dia $txt!";
+ 
+ $x = 7;
+ echo var_dump($nome);
+ $nome = "Senac";         
+ echo "<br>";
+ echo var_dump($x);
+ $salario = 1890.99;
+ echo "<br>";
+ echo var_dump($salario);
+ $ehestudante = true;
+ echo "<br>";
+ echo var_dump($ehestudante);
+?> 
+
+</body>
+</html>
---
+++ primeiroPHP.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+//Código PHP
+echo "<h1> Hello Word </h1>";
+echo "<h2>Segundo ano </h2>";
+echo "<h3>Senac</h3>";
+
+?> 
+
+</body>
+</html>

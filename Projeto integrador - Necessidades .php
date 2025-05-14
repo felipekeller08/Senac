bc7e1ea0869e0640b2fee92ebce8e85b4e24ffe0
felipekeller08 <felipeborile@gmail.com>
2025-05-14 10:17:54 -0300

    Add files via upload

---
+++ Projeto integrador - Necessidades .php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h1>Projeto integrador</h1>";
+$nome = "felipe";
+$auditiva = "não";
+$visual = "sim";
+$cognitivo = "não";
+echo "<h2>Entrevista</h2>";
+echo "<h3>você possui necessidade auditiva $nome?</h3>";
+echo "<br>";
+echo ($auditiva);
+echo "<br>";
+echo "<h3>você possui necessidade visual $nome?</h3>";
+echo "<br>";
+echo ($visual);
+echo "<br>";
+echo "<h3>você possui necessidade cognitiva $nome?</h3>";
+echo "<br>";
+echo ($cognitivo);
+
+?> 
+
+</body>
+</html>
